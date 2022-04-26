b21126dbc19f903c150dd7e8acfa66cc6696c6ff
SavvaKlimovitch <smklim2000@gmail.com>
2022-04-26 19:18:03 +0300

    Класс Main.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import ru.klimovitch.compmath.SixthSemester;
+
+public class Main {
+    public static void main(String... args) {
+        SixthSemester.tenthTheme8_9();
+        SixthSemester.eleventhTheme9_1b();
+        SixthSemester.eleventhTheme9_3b();
+    }
+}
